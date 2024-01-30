018b6e7da02296ab442bfe991f99c9f656391194
MAC <mac@MACs-MacBook-Air.local>
2024-01-30 12:16:15 +0100

    little edit

--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -6,34 +6,33 @@ import java.util.Scanner;
 public class testing {
     public static void main(String[] args) {
 
+
         Scanner reader = new Scanner(System.in);
         System.out.println("Please, enter Your name: ");
         String name = reader.nextLine();
         System.out.println("Welcome " + name + "!, Remember to Have Fun While Learning!");
 
         Random random = new Random();
-        int randomNum1 = random.nextInt(1, 50);
+        int randomNum1 = random.nextInt(1, 15);
         int randomNum2 = random.nextInt(1, 10);
         int result = 0;
 
         //Addtion
-        int sum = randomNum1 + randomNum2;
         System.out.println("What is the sum of " + randomNum1 + " and " + randomNum2);
         int userSum = reader.nextInt();
-
+        int sum = randomNum1 + randomNum2;
         if(userSum == sum) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer  The correct answer is " + sum);
+            System.out.println("Wrong answer.  The correct answer is " + sum);
         }
 
 
-        //Suntraction
-        int sub = randomNum1 - randomNum2;
+        //Subtraction
         System.out.println("What is the difference of " + randomNum1 + " and " + randomNum2);
         int userSub = reader.nextInt();
-
+        int sub = randomNum1 - randomNum2;
         if(userSub == sub) {
             System.out.println("Correct answer");
             result++;
@@ -42,25 +41,22 @@ public class testing {
         }
 
 
-
         //Multiplication
-        int mult = randomNum1 * randomNum2;
-        System.out.println("What is the sum of " + randomNum1 + " and " + randomNum2);
-        int userMult = reader.nextInt();
-
-        if(userMult == mult) {
+        System.out.println("What is the multiple of " + randomNum1 + " and " + randomNum2);
+        int userMultiply = reader.nextInt();
+        int multiply = randomNum1 * randomNum2;
+        if(userMultiply == multiply) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer! The correct answer is " + mult);
+            System.out.println("Wrong answer! The correct answer is " + multiply);
         }
 
 
         //Division
-        int div = randomNum1 / randomNum2;
         System.out.println("What is the division of " + randomNum1 + " and " + randomNum2);
         int userDiv = reader.nextInt();
-
+        int div = randomNum1 / randomNum2;
         if(userDiv == div) {
             System.out.println("Correct answer");
             result++;
@@ -73,7 +69,6 @@ public class testing {
         int mod = randomNum1 % randomNum2;
         System.out.println("What is the modulus of " + randomNum1 + " and " + randomNum2);
         int userMod = reader.nextInt();
-
         if(userMod == mod) {
             System.out.println("Correct answer!");
             result++;
@@ -86,7 +81,7 @@ public class testing {
 
 
         // final score
-        System.out.println( name + "Your Final Score: is " + result + " Keep Practicing");
+        System.out.println( name + "Your Final Score: is " + result + " out of 5 questions. Keep Practicing");
 
         reader.close();
     }
