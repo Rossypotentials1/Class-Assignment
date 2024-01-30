afaedbe13ee672ec454006c95759062131ae127c
MAC <mac@MACs-MacBook-Air.local>
2024-01-30 15:54:00 +0100

    Randomization

--- src/main/java/testing.java
+++ src/main/java/testing.java
@@ -10,7 +10,7 @@ public class testing {
         Scanner reader = new Scanner(System.in);
         System.out.println("Please, enter Your name: ");
         String name = reader.nextLine();
-        System.out.println("Welcome " + name + "!, Remember to Have Fun While Learning!");
+        System.out.println("Hello " + name + "!,Welcome to your Maths Assessment, Remember to Have Fun While Learning!");
 
         Random random = new Random();
         int randomNum1 = random.nextInt(1, 15);
@@ -18,62 +18,72 @@ public class testing {
         int result = 0;
 
         //Addtion
+        randomNum1 = random.nextInt(1, 25);
+        randomNum2 = random.nextInt(1, 10);
         System.out.println("What is the sum of " + randomNum1 + " and " + randomNum2);
         int userSum = reader.nextInt();
-        int sum = randomNum1 + randomNum2;
-        if(userSum == sum) {
+        int sumAnswer = randomNum1 + randomNum2;
+        if(userSum == sumAnswer) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer.  The correct answer is " + sum);
+            System.out.println("Wrong answer.  The correct answer is " + sumAnswer);
         }
 
 
         //Subtraction
+        randomNum1 = random.nextInt(1, 25);
+        randomNum2 = random.nextInt(1, 10);
         System.out.println("What is the difference of " + randomNum1 + " and " + randomNum2);
         int userSub = reader.nextInt();
-        int sub = randomNum1 - randomNum2;
-        if(userSub == sub) {
+        int subAnswer = randomNum1 - randomNum2;
+        if(userSub == subAnswer) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer ! The correct answer is " + sub);
+            System.out.println("Wrong answer ! The correct answer is " + subAnswer);
         }
 
 
         //Multiplication
-        System.out.println("What is the multiple of " + randomNum1 + " and " + randomNum2);
+        randomNum1 = random.nextInt(1, 25);
+        randomNum2 = random.nextInt(1, 10);
+        System.out.println("What is the multiplication of " + randomNum1 + " and " + randomNum2);
         int userMultiply = reader.nextInt();
-        int multiply = randomNum1 * randomNum2;
-        if(userMultiply == multiply) {
+        int multAnswer = randomNum1 * randomNum2;
+        if(userMultiply == multAnswer) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer! The correct answer is " + multiply);
+            System.out.println("Wrong answer! The correct answer is " + multAnswer);
         }
 
 
         //Division
+        randomNum1 = random.nextInt(1, 25);
+        randomNum2 = random.nextInt(1, 10);
         System.out.println("What is the division of " + randomNum1 + " and " + randomNum2);
         int userDiv = reader.nextInt();
-        int div = randomNum1 / randomNum2;
-        if(userDiv == div) {
+        int divAnswer = randomNum1 / randomNum2;
+        if(userDiv == divAnswer) {
             System.out.println("Correct answer");
             result++;
         } else {
-            System.out.println("Wrong answer! the correct answer is " + div);
+            System.out.println("Wrong answer! the correct answer is " + divAnswer);
         }
 
 
         //Modulus
-        int mod = randomNum1 % randomNum2;
+        randomNum1 = random.nextInt(1, 25);
+        randomNum2 = random.nextInt(1, 10);
         System.out.println("What is the modulus of " + randomNum1 + " and " + randomNum2);
         int userMod = reader.nextInt();
-        if(userMod == mod) {
+        int modAnswer = randomNum1 % randomNum2;
+        if(userMod == modAnswer) {
             System.out.println("Correct answer!");
             result++;
         } else {
-            System.out.println("Wrong answer!  The correct answer is " + mod);
+            System.out.println("Wrong answer!  The correct answer is " + modAnswer);
         }
 
 
@@ -81,7 +91,7 @@ public class testing {
 
 
         // final score
-        System.out.println( name + "Your Final Score: is " + result + " out of 5 questions. Keep Practicing");
+        System.out.println( name + " Your Final Score: is " + result + " out of 5 questions. Keep Practicing,Remember Practice makes perfect!");
 
         reader.close();
     }
